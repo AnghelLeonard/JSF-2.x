96c10ec7400483a01250c95905fb08ff50618e8a
AnghelLeonard <leoprivacy@yahoo.com>
2015-06-23 15:47:47 +0300

    Sample of using OmniFaces 2.1 CDI utility method, Beans#getQualifier()

---
+++ GetCDIQualifier/src/main/java/some/beans/ContextParam.java
@@ -0,0 +1,23 @@
+package some.beans;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({TYPE, METHOD, FIELD, PARAMETER})
+public @interface ContextParam {
+
+    @Nonbinding
+    String name() default "";
+
+}
