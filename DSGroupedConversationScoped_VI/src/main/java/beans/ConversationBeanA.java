56df6923efb2780504250bfc95138fe02970fce0
AnghelLeonard <leoprivacy@yahoo.com>
2015-12-13 11:56:39 +0200

    Closing all conversation groups

---
+++ DSGroupedConversationScoped_VI/src/main/java/beans/ConversationBeanA.java
@@ -0,0 +1,39 @@
+package beans;
+
+import java.io.Serializable;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.apache.deltaspike.core.api.scope.ConversationGroup;
+import org.apache.deltaspike.core.api.scope.GroupedConversation;
+import org.apache.deltaspike.core.api.scope.GroupedConversationScoped;
+
+@Named
+@GroupedConversationScoped
+@ConversationGroup(ConversationABInterface.class)
+public class ConversationBeanA implements Serializable {
+
+    @Inject
+    private GroupedConversation conversation;
+
+    private int A;
+
+    public ConversationBeanA() {
+        A = 0;
+    }
+
+    public void increaseA() {
+        A++;        
+    }
+
+    public void finish() {
+        this.conversation.close();
+    }
+
+    public int getA() {
+        return A;
+    }
+
+    public void setA(int A) {
+        this.A = A;
+    }
+}
---
+++ DSGroupedConversationScoped_VI/src/main/java/beans/OutsideBeanCD.java
@@ -0,0 +1,19 @@
+package beans;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.apache.deltaspike.core.spi.scope.conversation.GroupedConversationManager;
+
+@Named
+@RequestScoped
+public class OutsideBeanCD implements Serializable {
+
+    @Inject
+    private GroupedConversationManager conversationManager;
+
+    public void finish() {
+        this.conversationManager.closeConversationGroup(ConversationCDInterface.class);
+    }
+}
