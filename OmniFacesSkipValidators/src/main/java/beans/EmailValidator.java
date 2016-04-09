89b3ea726169eb5d1aaac1bc153e5c111095c689
AnghelLeonard <leoprivacy@yahoo.com>
2016-04-09 08:23:58 +0300

    OmniFaces SkipValidators Example

---
+++ OmniFacesSkipValidators/src/main/java/beans/EmailValidator.java
@@ -0,0 +1,41 @@
+package beans;
+
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator
+public class EmailValidator implements Validator {
+
+    private static final String EMAIL_PATTERN
+            = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+        String email = value.toString();
+
+        if (!isValidEmail(email)) {            
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid e-mail!", null);
+            throw new ValidatorException(message);
+        }
+    }
+
+    /**
+     *
+     * Helper method for #validate()
+     */
+    private boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+}
